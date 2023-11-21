db1a30fc69c0ae74e83a5069f7751c4efa06c5f7
fiqriwahyu <fiqriwahyu58@gmail.com>
2023-11-21 21:45:32 +0700

    fix img

--- script.js/contact.js
+++ script.js/contact.js
@@ -1,25 +1,25 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwPQYINk-ZGHJK1F2X1BtRknkKzov3JDuVDehzPi71a_G2i1KURI4uPJeUhGy-DSnbT/exec';
-      const form = document.forms['Revou-contact'];
-      const BtnKirim = document.querySelector('.btn-kirim');
-      const BtnLoading = document.querySelector('.btn-loading');
-      const Alert = document.querySelector('.alert');
+const form = document.forms['Revou-contact'];
+const BtnKirim = document.querySelector('.btn-kirim');
+const BtnLoading = document.querySelector('.btn-loading');
+const Alert = document.querySelector('.alert');
 
-      form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        //ketika tombol submit di klik
-        // tampilkan tombol loading, dan menghilangkan tombol kirim
-        BtnLoading.classList.toggle('d-none'); //cari loading cek, toggle(switch ada jadi tidak ada dan sebaliknya)
-        BtnKirim.classList.toggle('d-none');
-        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-          .then((response) => {
-            // tampilkan tombol kirim, dan menghilangkan tombol loading
-            BtnLoading.classList.toggle('d-none');
-            BtnKirim.classList.toggle('d-none');
-            //tampilkan alert
-            Alert.classList.toggle('d-none');
-            //reset formnya
-            form.reset();
-            console.log('Success!', response);
-          })
-          .catch((error) => console.error('Error!', error.message));
-      });
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //ketika tombol submit di klik
+  // tampilkan tombol loading, dan menghilangkan tombol kirim
+  BtnLoading.classList.toggle('d-none'); //cari loading cek, toggle(switch ada jadi tidak ada dan sebaliknya)
+  BtnKirim.classList.toggle('d-none');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => {
+      // tampilkan tombol kirim, dan menghilangkan tombol loading
+      BtnLoading.classList.toggle('d-none');
+      BtnKirim.classList.toggle('d-none');
+      //tampilkan alert
+      Alert.classList.toggle('d-none');
+      //reset formnya
+      form.reset();
+      console.log('Success!', response);
+    })
+    .catch((error) => console.error('Error!', error.message));
+});
--- script.js/pay.js
+++ script.js/pay.js
@@ -1,9 +1,9 @@
 const pop = {
-    pop: document.getElementById('settingssett'),
-    popup() {
-      this.pop.style.display = 'block';
-    },
-    popClear() {
-      this.pop.style.display = 'none';
-    },
-  };
+  pop: document.getElementById('settingssett'),
+  popup() {
+    this.pop.style.display = 'block';
+  },
+  popClear() {
+    this.pop.style.display = 'none';
+  },
+};
