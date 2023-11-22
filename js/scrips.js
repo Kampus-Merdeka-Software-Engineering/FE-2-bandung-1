09adc5fbe367490779f85205cae70530025eab78
fiqriwahyu <fiqriwahyu58@gmail.com>
2023-11-22 23:09:15 +0700

    +buger nav

---
+++ js/scrips.js
@@ -0,0 +1,6 @@
+const head = document.getElementById('header');
+const burger = document.getElementById('burger');
+
+burger.addEventListener('click', function () {
+  head.classList.toggle('header-active');
+});
